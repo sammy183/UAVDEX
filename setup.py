9c99986b56c3975198415bc7a8b996eebafb7763
sammy183 <60051551+sammy183@users.noreply.github.com>
2026-01-18 15:31:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,16 +29,11 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
 
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.9',
     # install_requires=['Pillow'],
     extras_require={
         'dev': ['check-manifest'],
