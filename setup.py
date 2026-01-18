0800501d1836c37d55acbe2bb69d7689d2c417b2
sammy183 <60051551+sammy183@users.noreply.github.com>
2026-01-18 15:41:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,21 +4,17 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='example_pypi_package',
-    author='Tom Chen',
-    author_email='tomchen.org@gmail.com',
-    description='Example PyPI (Python Package Index) Package',
-    keywords='example, pypi, package',
+    name = "uavdex",
+    author='Samuel Nassau',
+    author_email='sammynassau@gmail.com',
+    description='Unmanned Aerial Vehicle Design EXploration.\nA Python package for multidisciplinary analysis and optimization of electric group 1-2 UAVs.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/tomchen/example_pypi_package',
+    url='https://github.com/sammy183/UAVDEX',
     project_urls={
-        'Documentation': 'https://github.com/tomchen/example_pypi_package',
-        'Bug Reports':
-        'https://github.com/tomchen/example_pypi_package/issues',
-        'Source Code': 'https://github.com/tomchen/example_pypi_package',
-        # 'Funding': '',
-        # 'Say Thanks!': '',
+        'Documentation':'',
+        'Bug Reports':'',
+        'Source Code':'',
     },
     package_dir={'': 'src'},
     packages=setuptools.find_packages(where='src'),
@@ -34,7 +30,12 @@ setuptools.setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.9',
-    # install_requires=['Pillow'],
+    install_requires=["matplotlib",
+                    "numpy",
+                    "scipy",
+                    "gekko",
+                    "pandas",
+                    "tqdm"],
     extras_require={
         'dev': ['check-manifest'],
         # 'test': ['coverage'],
