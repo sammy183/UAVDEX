f600367a2dde410a82de30ee76ca63256fe6d55d
sammy183 <60051551+sammy183@users.noreply.github.com>
2026-03-22 17:52:32 -0400

    auto version num

--- src/uavdex/__init__.py
+++ src/uavdex/__init__.py
@@ -1,4 +1,14 @@
-__version__ = "0.1.4"
-
 from pathlib import Path
 _uavdex_root = Path(__file__).parent
+
+from uavdex.common import *
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # For Python < 3.8
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("uavdex")
+except PackageNotFoundError:
+    __version__ = "unknown"
