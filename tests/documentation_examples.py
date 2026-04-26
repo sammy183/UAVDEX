0b81c9d7c6f65ff795df4485bda529f3bcc07733
sammy183 <60051551+sammy183@users.noreply.github.com>
2026-04-26 18:06:05 -0400

    .

--- tests/documentation_examples.py
+++ tests/documentation_examples.py
@@ -35,48 +35,48 @@ import numpy as np
 
 # Component Initialization
 design = ud.PointDesign() 				# initialize PointDesign object
-design.Motor('C-4130/20', nmot = 2)		# add a motor, and specify the # of motors
+design.Motor('C-4130/20', nmot = 1)		# add a motor, and specify the # of motors
 design.Battery('Gaoneng_8S_3300') 		# add a battery 
-design.Prop('16x10E') 					# add a propeller
+design.Prop('18x12E') 					# add a propeller
 
 # to control the number of points used in linspace (n = 50 --> ~5s runtime, n = 200 --> ~15s runtime)
 n = 120  
 
-# ContourPlot (sweeps of velocity and runtime)
-design.ContourPlot(propQ = ['T_lbf', 'eta_drive', 'Ib'],
-                   Uinf_mps = np.linspace(0, 45, n), 
-                   t_s = np.linspace(0, 300, n),
-                   dT = 100, 
-                   h_m = 100)
+# # ContourPlot (sweeps of velocity and runtime)
+# design.ContourPlot(propQ = ['T_lbf', 'eta_drive', 'Ib'],
+#                    Uinf_mps = np.linspace(0, 45, n), 
+#                    t_s = np.linspace(0, 300, n),
+#                    dT = 100, 
+#                    h_m = 100)
 
-#%% Additional contourplot
-design.ContourPlot(propQ = 'eta_drive',
-                  Uinf_mph = np.linspace(0, 120, 300), 
-                  dT = np.linspace(20, 100, 300), 
-                  h_m = 50, 
-                  SOC = 80)
+# #%% Additional contourplot
+# design.ContourPlot(propQ = 'eta_drive',
+#                   Uinf_mph = np.linspace(0, 120, 300), 
+#                   dT = np.linspace(20, 100, 300), 
+#                   h_m = 50, 
+#                   SOC = 80)
 
 design.ContourPlot(propQ = 'eta_drive',
-                  Uinf_mps = np.linspace(0, 45, 200), 
+                  Uinf_mps = np.linspace(0, 45, 300), 
                   dT = 100,
                   h_m = 50, 
-                  Voc = np.linspace(3.5, 4.2, 200))
+                  Voc = np.linspace(3.5, 4.2, 300))
 
 design.ContourPlot(propQ = 'eta_drive',
                   Uinf_mps = 30.0,
-                  dT = np.linspace(20, 100, 200),
+                  dT = np.linspace(20, 100, 300),
                   h_m = 50, 
-                  Voc = np.linspace(3.5, 4.2, 200),
+                  Voc = np.linspace(3.5, 4.2, 300),
                   CD = 0.03,
                   Sw_m2 = 0.65)
 
 #%% Altitude Efficiency Examination
 n = 300
-design.ContourPlot(propQ = ['eta_drive', 'eta_p', 'eta_m', 'RPM', 'Q_Nm'],
-                   Uinf_kt = np.linspace(0, 90, n),
-                   dT = 80,
-                   h_ft = np.linspace(0, 80000, n),
-                   t_s = 30)
+# design.ContourPlot(propQ = ['eta_drive', 'eta_p', 'eta_m', 'RPM', 'Q_Nm'],
+#                    Uinf_kt = np.linspace(0, 90, n),
+#                    dT = 80,
+#                    h_ft = np.linspace(0, 80000, n),
+#                    t_s = 30)
 #%% All 6 styles for t input
 # n = 300 # overview of all 6 types of contourplot
 # plot = True
@@ -88,9 +88,10 @@ design.ContourPlot(propQ = ['eta_drive', 'eta_p', 'eta_m', 'RPM', 'Q_Nm'],
 # dT_const = 80
 # h_arr = np.linspace(0, 80000, n)
 # h_const = 100
-# CD = None#0.045
-# Sw_m2 = None#0.81
-# cases = [{"propQ":"eta_drive",
+# CD = 0.03
+# Sw_m2 = 0.6
+# propQ = 'eta_drive'
+# cases = [{"propQ":propQ,
 #           "t_s":t_arr,
 #           "Uinf_mps":Uinf_arr, 
 #           "dT":dT_const,
@@ -99,7 +100,7 @@ design.ContourPlot(propQ = ['eta_drive', 'eta_p', 'eta_m', 'RPM', 'Q_Nm'],
 #           "CD":CD,
 #           "Sw_m2":Sw_m2},
          
-#           {"propQ":"eta_drive",
+#         {"propQ":propQ,
 #         "t_s":t_arr,
 #         "Uinf_mps":Uinf_const, 
 #         "dT":dT_arr,
@@ -108,7 +109,7 @@ design.ContourPlot(propQ = ['eta_drive', 'eta_p', 'eta_m', 'RPM', 'Q_Nm'],
 #         "CD":CD,
 #         "Sw_m2":Sw_m2},
          
-#           {"propQ":"eta_drive",
+#         {"propQ":propQ,
 #         "t_s":t_arr,
 #         "Uinf_mps":Uinf_const, 
 #         "dT":dT_const,
@@ -117,7 +118,7 @@ design.ContourPlot(propQ = ['eta_drive', 'eta_p', 'eta_m', 'RPM', 'Q_Nm'],
 #         "CD":CD,
 #         "Sw_m2":Sw_m2},
          
-#           {"propQ":"eta_drive",
+#         {"propQ":propQ,
 #         "t_s":t_const,
 #         "Uinf_mps":Uinf_arr, 
 #         "dT":dT_arr,
@@ -126,7 +127,7 @@ design.ContourPlot(propQ = ['eta_drive', 'eta_p', 'eta_m', 'RPM', 'Q_Nm'],
 #         "CD":CD,
 #         "Sw_m2":Sw_m2},
          
-#           {"propQ":"eta_drive",
+#         {"propQ":propQ,
 #         "t_s":t_const,
 #         "Uinf_mps":Uinf_arr, 
 #         "dT":dT_const,
@@ -135,7 +136,7 @@ design.ContourPlot(propQ = ['eta_drive', 'eta_p', 'eta_m', 'RPM', 'Q_Nm'],
 #         "CD":CD,
 #         "Sw_m2":Sw_m2},
          
-#           {"propQ":"eta_drive",
+#         {"propQ":propQ,
 #         "t_s":t_const,
 #         "Uinf_mps":Uinf_const, 
 #         "dT":dT_arr,
